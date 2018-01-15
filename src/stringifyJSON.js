ba66a7b92fc8330270ccec067ab8abbb40c8bd5b
nicola24 <nicolas.peyrichou@gmail.com>
2018-01-15 12:26:44 -0800

    pair working

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,54 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  let results = [];
+
+  if (Array.isArray(obj) === true) {
+    obj.forEach((elements) => results.push(stringifyJSON(elements)));
+    return `[${results.join(',')}]`;
+  }
+
+  if (typeof obj === 'object') {
+    if (obj === null) {
+      return 'null';
+    } else { 
+      for (var key in obj) {
+        if (typeof obj[key] !== 'function' || !obj.hasOwnProperty(undefined)) {
+          results.push(`${stringifyJSON(key)}:${stringifyJSON(obj[key])}`);
+        }
+      }
+      return '{' + results + '}';
+    }
+  }
+
+
+  if (typeof obj === 'string') {
+    return `"${obj}"`;
+  }
+
+  if (typeof obj === 'number', 'boolean', 'string') {
+    return '' + obj;
+  }
+
 };
+
+
+
+//    {},
+//   {'a': 'apple'},
+//   {'foo': true, 'bar': false, 'baz': null},
+//   {'boolean, true': true, 'boolean, false': false, 'null': null },
+//   // basic nesting
+//   {'a': {'b': 'c'}},
+//   {'a': ['b', 'c']},
+//   [{'a': 'b'}, {'c': 'd'}],
+//   {'a': [], 'c': {}, 'b': true}
+  // {
+  //   'functions': function() {},
+  //   'undefined': undefined
+  // }
+
+
+
+
+
