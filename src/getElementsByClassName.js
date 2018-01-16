311a5830577388d71ac518575b64a2023d0f2ad7
nicola24 <nicolas.peyrichou@gmail.com>
2018-01-15 18:09:14 -0800

    edited code to include element.childNode as per requirement.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,16 +3,6 @@
 //   return document.getElementsByClassName(className);
 // };
 
-// var htmlStrings = [
-//   '<div class="targetClassName"></div>',
-//   '<div class="otherClassName targetClassName"></div>',
-//   '<div><div class="targetClassName"></div></div>',
-//   '<div><div class="targetClassName"><div class="targetClassName"></div></div></div>',
-//   '<div><div></div><div><div class="targetClassName"></div></div></div>',
-//   '<div><div class="targetClassName"></div><div class="targetClassName"></div></div>',
-//   '<div><div class="somediv"><div class="innerdiv"><span class="targetClassName">yay</span></div></div></div>'
-// ];
-
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
 
@@ -21,14 +11,15 @@ var getElementsByClassName = function(className) {
   var nodes = [];
   var searchNodes = function(element) {
     
-    // document.body contains classname then push it to array nodes
-    if (element.classList.contains(className)) {
+    //check if className is within the node
+    if (typeof element.classList !== 'undefined' && element.classList.contains(className)) {
       nodes.push(element);
     }
     
-    // for (var i = 0; i < element.children.length; i++) {
-    //   searchNodes(element.children[i]);
-    // }
+    //iterate through each childNodes
+    if (element.childNodes) {
+      Object.keys(element.childNodes).forEach((key) => searchNodes(element.childNodes[key]));
+    }
   };
 
   searchNodes(document.body);
